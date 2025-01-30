03d0d87a57eb175b97c9d5e1ca2b5f207a02536d
Ben Williams <brwilliams@phillips.com>
2025-01-30 10:09:30 -0500

    Initial Commit

---
+++ PerceptionBindableTest/PerceptionBindableTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerceptionBindableTestApp.swift
+//  PerceptionBindableTest
+//
+//  Created by Ben Williams on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct PerceptionBindableTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PerceptionBindableTestTests/PerceptionBindableTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  PerceptionBindableTestTests.swift
+//  PerceptionBindableTestTests
+//
+//  Created by Ben Williams on 1/30/25.
+//
+
+import Testing
+@testable import PerceptionBindableTest
+
+struct PerceptionBindableTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
